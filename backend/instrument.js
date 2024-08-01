a870008feb6182d19d2e399396daaf08e0707ae8
justsoul12 <fetih.girma1@gmail.com>
2024-08-01 12:39:23 +0100

    clerk and sentry configurations made for 1st time

---
+++ backend/instrument.js
@@ -0,0 +1,15 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+const Sentry = require("@sentry/node");
+const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+
+Sentry.init({
+  dsn: "https://b6c26be524a9282cfd41b26521d3af88@o4507700831518720.ingest.us.sentry.io/4507700840038400",
+  integrations: [
+    nodeProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
