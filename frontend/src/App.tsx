182c819a6ed0021a7d5637714cbbb242617536f5
alazaradane <alazargithub369@gmail.com>
2024-08-02 14:38:04 +0300

    update auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
 
 
 export default function App() {
   return (
     <header>
       <SignedOut>
-        <SignInButton />
+        <Button className="mx-10">
+         <SignInButton />
+        </Button>
       </SignedOut>
       <SignedIn>
         <UserButton />
